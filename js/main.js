5948758734a69b01586b07cf55bffb34e72f310e
almascanio <almascanio08@gmail.com>
2023-08-17 15:13:57 -0300

    Commit/Entrega proyecto final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const menuListas = document.querySelectorAll(".menu-li");
-const menuButton = document.querySelector("#menu-button")
+const menuButton = document.querySelector("#menu-button");
+const slider = document.querySelector('.slider');
 
 menuListas.forEach(function(lista){
     lista.addEventListener("click", function(e){
@@ -13,3 +14,12 @@ menuButton.addEventListener("click", function(){
     document.body.classList.toggle("menu-mobile")
 });
 
+window.addEventListener('load', () => {
+    const loading = document.getElementById('loading');
+    const content = document.getElementById('content');
+
+    setTimeout(() => {
+        loading.style.display = 'none';
+        content.classList.remove('hidden');
+    }, 800);
+});
